5bd6d446b769520d99edfeeebfc0ad960aabfff7
Ebee02 <beelawrence8@gmail.com>
2025-01-26 18:46:40 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+  
+return (
+    <>
+     <section className="bg-white min-h-screen w-full">
+     <main className="border-2 border-black max-w-[1100px] mx-16 mt-2">
+      <Navbar/>
+      fjdskjfdklfjdkljfdlkjfkldj
+      </main>
+     </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <h3>logo placement</h3>
+
+        <ul><li><a href="#"></a></li></ul>
+    </header>
+  )
+}
+
+export default Navbar
