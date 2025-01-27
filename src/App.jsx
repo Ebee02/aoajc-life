8bcad87755198aa7aa3cc2eebd98766edccb76f7
Ebee02 <beelawrence8@gmail.com>
2025-01-27 07:35:17 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import Navbar from "./components/Navbar";
+import placementImg from "./assets/images/placement-image.jpg";
 
 function App() {
   return (
     <>
-      <section className="bg-white min-h-screen w-full">
-        <main className="max-w-[1100px] mx-20">
+      <section className="bg-white min-h-screen w-full grid place-items-center">
+        <main className="max-w-[990px]">
           <Navbar />
+          <div className="grid grid-cols-2 gap-4 h-[400px]">
+            <div>
+              <div className="text-7xl font-bold uppercase w-full tracking-wide">
+                At <span className="text-[#A36A28]">AOAJC, </span>
+                We preach <br />
+                the bible
+              </div>
+              <div className="mt-5 flex gap-4">
+                <div className="w-10 h-[160px] bg-black rounded-md"></div>
+                <p className="text-lg">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+                  totam animi sit accusantium itaque consequatur minus, laborum
+                  omnis consequuntur quibusdam impedit dignissimos quos quas
+                  voluptatum neque numquam sed tempore in!
+                </p>
+              </div>
+            </div>
+            <img
+              src={placementImg}
+              alt="pastor img"
+              className="w-full h-[400px] rounded-md"
+            />
+          </div>
         </main>
       </section>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,28 +3,28 @@ import { FaYoutube, FaFacebook, FaWhatsapp } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <header className="flex items-center justify-between pt-4">
+    <header className="flex items-center justify-between mb-10 ">
       <h3 className="text-xl font-semibold tracking-wide">logo placement</h3>
 
-      <ul className="flex items-center gap-6 py-3">
+      <ul className="flex items-center gap-6">
         <li>
           <a href="#">
-            <FaYoutube className="cursor-pointer duration-200 transform text-2xl hover:scale-150" />
+            <FaYoutube className="cursor-pointer duration-200 transform text-2xl hover:scale-150 hover:text-[#A36A28]" />
           </a>
         </li>
         <li>
           <a href="#">
-            <FaFacebook className="cursor-pointer duration-200 transform text-2xl hover:scale-150" />
+            <FaFacebook className="cursor-pointer duration-200 transform text-2xl hover:scale-150 hover:text-[#A36A28]" />
           </a>
         </li>
         <li>
           <a href="#">
-            <MdEmail className="cursor-pointer duration-200 transform text-2xl hover:scale-150" />
+            <MdEmail className="cursor-pointer duration-200 transform text-2xl hover:scale-150 hover:text-[#A36A28]" />
           </a>
         </li>
         <li>
           <a href="#">
-            <FaWhatsapp className="cursor-pointer duration-200 transform text-2xl hover:scale-150" />
+            <FaWhatsapp className="cursor-pointer duration-200 transform text-2xl hover:scale-150 hover:text-[#A36A28]" />
           </a>
         </li>
       </ul>
